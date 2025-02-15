2d00322f0fdacb95300ecba785cadcceee19006b
akibaz <bavakumovic@gmail.com>
2025-02-15 20:15:41 +0100

    Create model, controller, service classes. Setup postgres db and pgadmin docker containers. Configure postgres jdbc and jpa hibernate for db interaction. Expose REST api for registering new user.

---
+++ customer/src/main/java/dev/akibaz/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,7 @@
+package dev.akibaz.customer;
+
+public record CustomerRegistrationRequest(
+        String firstName,
+        String lastName,
+        String email) {
+}
---
+++ customer/src/main/java/dev/akibaz/customer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package dev.akibaz.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
