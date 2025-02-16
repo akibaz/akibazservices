386025a9887095fccfa3f1a777533b23e2e4ea32
akibaz <bavakumovic@gmail.com>
2025-02-16 21:53:51 +0100

    Connect customer and fraud services with rest template

--- customer/src/main/java/dev/akibaz/customer/CustomerService.java
+++ customer/src/main/java/dev/akibaz/customer/CustomerService.java
@@ -1,9 +1,15 @@
 package dev.akibaz.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer newCustomer = Customer.builder()
@@ -13,6 +19,18 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .build();
         // todo: check if email valid
         // todo: check if email not taken
-        customerRepository.save(newCustomer);
+        // insert customer in db
+        customerRepository.saveAndFlush(newCustomer);
+        // todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                newCustomer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("Customer with id " + newCustomer.getId() + " is already fraudster");
+        }
+        // todo: send notification
     }
 }
---
+++ customer/src/main/java/dev/akibaz/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package dev.akibaz.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
